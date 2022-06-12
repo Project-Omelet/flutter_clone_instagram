c1d5e73cef72a5c6d4dddbf68acf8a34fd1b47fb
Luke Lee <hslee89@icloud.com>
2022-06-12 14:08:15 +0900

    feat: Add active UI

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_instagram/src/components/image_data.dart';
 import 'package:flutter_clone_instagram/src/controller/bottom_nav_controller.dart';
+import 'package:flutter_clone_instagram/src/pages/active_history.dart';
 import 'package:flutter_clone_instagram/src/pages/home.dart';
 import 'package:flutter_clone_instagram/src/pages/search.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
@@ -22,12 +23,8 @@ class App extends GetView<BottomNavController> {
                       builder: (context) => const Search());
                 },
               ),
-              Container(
-                child: Center(child: Text('UPLOAD')),
-              ),
-              Container(
-                child: Center(child: Text('ACTIVITY')),
-              ),
+              Container(),
+              const ActiveHistory(),
               Container(
                 child: Center(child: Text('MYPAGE')),
               ),
---
+++ lib/src/pages/active_history.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clone_instagram/src/components/avatar_widget.dart';
+
+class ActiveHistory extends StatelessWidget {
+  const ActiveHistory({Key? key}) : super(key: key);
+
+  Widget _activeitemOne() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: const [
+          AvatarWidget(
+              type: AvatarType.type2,
+              size: 40,
+              thumbPath:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ur_QI0GejFNytcFOs2sudVKBhAuGFdq9eA&usqp=CAU'),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Text.rich(
+              TextSpan(
+                text: '섭멍님',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                    text: '님이 회원님의 게시물을 좋아합니다. 엄청나게 좋아하는 것 같아요!!!!',
+                    style: TextStyle(fontWeight: FontWeight.normal),
+                  ),
+                  TextSpan(
+                    text: ' 5 일전',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 13,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _newRecentlyActiveView(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          _activeitemOne(),
+          _activeitemOne(),
+          _activeitemOne(),
+          _activeitemOne(),
+          _activeitemOne(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text(
+          '활동',
+          style: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _newRecentlyActiveView('오늘'),
+            _newRecentlyActiveView('이번주'),
+            _newRecentlyActiveView('이번달'),
+          ],
+        ),
+      ),
+    );
+  }
+}
